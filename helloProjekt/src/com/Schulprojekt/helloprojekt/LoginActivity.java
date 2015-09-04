599a3ad99e3003b5bbf7057a6698fac62ea3dd3c
rehdti <tim-rehders@web.de>
2015-09-04 12:13:27 +0200

    rename

--- helloProjekt/src/com/Schulprojekt/helloprojekt/LoginActivity.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/LoginActivity.java
@@ -8,47 +8,49 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+public class LoginActivity extends Activity{
 
-public class LoginActivity extends Activity {
+	public Button button1;
 	
-	Button bregistrieren;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_registration);
+		
+		button1 = (Button) findViewById(R.id.button1);
+		button1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (v == button1){
+					button1.setText("Yess!");
+				}
+				
+			}
+		});
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        
-        bregistrieren = (Button) findViewById(R.id.button1);
-        bregistrieren.setOnClickListener(new OnClickListener() {
-        	
-        	public void onClick(View view) {
-        		
-        		if(view == bregistrieren) {
-        			bregistrieren.setText("blabla");
-        		}       					
-        	}
-        });
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.registration, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-
-    
-
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onClickListener(){
+		
+	}
 }
---
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/RegistrationActivity.java
@@ -0,0 +1,54 @@
+package com.Schulprojekt.helloprojekt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class RegistrationActivity extends Activity {
+	
+	Button bregistrieren;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        bregistrieren = (Button) findViewById(R.id.button1);
+        bregistrieren.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View view) {
+        		
+        		if(view == bregistrieren) {
+        			bregistrieren.setText("blabla");
+        		}       					
+        	}
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+
+    
+
+}
