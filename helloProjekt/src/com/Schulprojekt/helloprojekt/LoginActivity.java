f99957779d304ce09ba2dd6698089eba3d4a3565
kneska <k.knesel@web.de>
2015-09-04 11:51:55 +0200

    login

--- helloProjekt/src/com/Schulprojekt/helloprojekt/LoginActivity.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/LoginActivity.java
@@ -4,14 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class LoginActivity extends Activity {
+	
+	Button bregistrieren;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        bregistrieren = (Button) findViewById(R.id.button1);
+        bregistrieren.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View view) {
+        		
+        		if(view == bregistrieren) {
+        			bregistrieren.setText("blabla");
+        		}       					
+        	}
+        });
     }
 
     @Override
@@ -31,4 +47,8 @@ public class LoginActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+
+    
+
 }
