aefd965415cebf841302bae7eab69572ac0b8973
kneska <k.knesel@web.de>
2015-09-15 14:11:08 +0200

    test

--- helloProjekt/src/com/Schulprojekt/helloprojekt/RegistrationActivity.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/RegistrationActivity.java
@@ -1,5 +1,15 @@
 package com.Schulprojekt.helloprojekt;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,32 +17,145 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class RegistrationActivity extends Activity {
 	
+	private final static String SERVICE_URI = "http://";
+	
 	Button bregistrieren;
+	JSONObject user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         
-        bregistrieren = (Button) findViewById(R.id.login);
+        bregistrieren = (Button) findViewById(R.id.registrieren);
         bregistrieren.setOnClickListener(new OnClickListener() {
         	
         	public void onClick(View view) {
         		
         		if(view == bregistrieren) {
-        			bregistrieren.setText("blabla");
-        		}       					
+        			EditText tvname = (EditText) findViewById(R.id.username);
+        			EditText tvalias = (EditText) findViewById(R.id.username);
+        			EditText tvpasswort = (EditText) findViewById(R.id.passwort);
+        			EditText tvpasswortwh = (EditText) findViewById(R.id.passwortwh);
+        			
+        			String name = tvname.getText().toString();
+        			String alias = tvalias.getText().toString();
+        			String passwort = tvpasswort.getText().toString();
+        			String passwortwh = tvpasswortwh.getText().toString();
+        			
+        			
+
+        			if(name.equals("") || name.equals(null) || alias.equals("") || alias.equals(null) || passwort.equals("") || passwort.equals(null)){
+        				Toast.makeText(RegistrationActivity.this, "Es werden alle Eingaben Benötigt", Toast.LENGTH_LONG).show();
+        			}else{
+        				if(passwortwh.equals(passwort)){
+        					
+        					
+        					DefaultHttpClient httpClient = new DefaultHttpClient();
+        			        HttpGet request = new HttpGet(SERVICE_URI + "/GetUserByAccountName");
+        			 
+        			        request.setHeader("Accept", "application/json");
+        			        request.setHeader("Content-type", "application/json");
+        			 
+        			        HttpResponse response;
+							try {
+								response = httpClient.execute(request);
+        			 
+        			        HttpEntity responseEntity = response.getEntity();
+        			 
+        			        // Read response data into buffer
+        			        char[] buffer = new char[(int)responseEntity.getContentLength()];
+        			        InputStream stream = responseEntity.getContent();
+        			        InputStreamReader reader = new InputStreamReader(stream);
+        			        reader.read(buffer);
+        			        stream.close();
+        			 
+        			        user = new JSONObject(new String(buffer));
+
+							} catch (ClientProtocolException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+        					
+        					if(user.isNull(name)){
+        						Toast.makeText(RegistrationActivity.this, "Name schon vergeben!", Toast.LENGTH_LONG).show();
+        					}else{
+        					
+        							
+        				
+//        					new User(name, alias, passwort, true);
+//        					
+//        					HttpPost request = new HttpPost(SERVICE_URI + "/CreateUser");
+//        		            request.setHeader("Accept", "application/json");
+//        		            request.setHeader("Content-type", "application/json");
+//        		 
+//        		            // Build JSON string
+//        		            JSONStringer user;
+//							try {
+//								user = new JSONStringer()
+//								    .object()
+//								        .key("user")
+//								            .object()
+//								                .key("userId").value(null)
+//								                .key("accountName").value(name)
+//								                .key("accountState").value("")
+//								                .key("expierencePoints").value(0)
+//								                .key("picture").value(new byte[]{50})
+//								                .key("password").value(passwort.getBytes())
+//								            .endObject()
+//								        .endObject();
+//        		            StringEntity entity = new StringEntity(user.toString());
+//        		 
+//        		            request.setEntity(entity);
+//        		 
+//        		            // Send request to WCF service
+//        		            DefaultHttpClient httpClient = new DefaultHttpClient();
+//        		            HttpResponse response = httpClient.execute(request);
+//        		            
+//        		            Log.d("WebInvoke", "Saving : " + response.getStatusLine().getStatusCode());
+//
+//							} catch (JSONException e) {
+//								// TODO Auto-generated catch block
+//								e.printStackTrace();
+//							} catch (UnsupportedEncodingException e) {
+//								// TODO Auto-generated catch block
+//								e.printStackTrace();
+//							} catch (ClientProtocolException e) {
+//								// TODO Auto-generated catch block
+//								e.printStackTrace();
+//							} catch (IOException e) {
+//								// TODO Auto-generated catch block
+//								e.printStackTrace();
+//							}
+
+        				
+        					}
+        				}
+        				else{
+        					Toast.makeText(RegistrationActivity.this, "Passwörter stimmen nicht ueberein", Toast.LENGTH_LONG).show();
+        				}
+
+        			} 
+        			}       					
         	}
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.login, menu);
+        getMenuInflater().inflate(R.menu.registration, menu);
         return true;
     }
 
