8e602ebf48e863f1407426585da426c6a7dd4e06
it3-stehch <it3-stehch@R208-22.g18.de>
2015-09-17 11:30:07 +0200

    .

--- helloProjekt/src/com/Schulprojekt/helloprojekt/ContactListActivity.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/ContactListActivity.java
@@ -6,17 +6,15 @@ import com.Schulprojekt.helloprojekt.GUILogik.ContactListEntry;
 import com.Schulprojekt.helloprojekt.GUILogik.ContactListLogik;
 import com.Schulprojekt.helloprojekt.GUILogik.User;
 
-import android.R.color;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.Application;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -28,42 +26,26 @@ public class ContactListActivity extends Activity {
 	ArrayList<ContactListEntry> contactList;
 	User u1 = new User("test", "test", "test",true);
 	User u2 = new User("test", "test", "test",true);
-	ImageView imgV;
+	ContactListLogik conlog = new ContactListLogik();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact_list);
-		
-		//contactList = conlog.fillList(userList, this);
-		imgV = (ImageView) findViewById(R.id.imageView);
 		final LinearLayout linlayoutVertical = (LinearLayout) findViewById(R.id.linLayoutContactVertical);
 		final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollViewContact);
 		final TextView textV = (TextView) findViewById(R.id.textViewContact);
-		
-		imgV.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent in = new Intent(ContactListActivity.this, ChatActivity.class);
-				in.putExtra("ContactImage", (CharSequence) imgV.getBackground());
-				startActivity(in);
-				
-//				ContactListLogik conlog = new ContactListLogik();
-//				userList.add(u1);
-//				userList.add(u2);
-//				contactList = conlog.fillList(userList, getApplicationContext());
-//				textV.setText("test");
-//				for (ContactListEntry contact : contactList) {
-//					LinearLayout lilayout = contact.getLinlayout();
-//					lilayout.setOrientation(LinearLayout.HORIZONTAL);
-//					contact.getContactPicture().setOnClickListener(conlog.getOnKlickListener(contact.getContactPicture()));
-//					lilayout.addView(contact.getContactPicture());
-//					lilayout.addView(contact.getAlias());
-//					linlayoutVertical.addView(lilayout);
-//				}
-			}
-		});
+		userList.add(u1);
+		userList.add(u2);
+		contactList = conlog.fillList(userList, getApplicationContext());
+		for (ContactListEntry contact : contactList) {
+			LinearLayout lilayout = contact.getLinlayout();
+			lilayout.setOrientation(LinearLayout.HORIZONTAL);
+			contact.getContactPicture().setOnClickListener(getOnKlickListener(contact.getContactPicture()));
+			lilayout.addView(contact.getContactPicture());
+			lilayout.addView(contact.getAlias());
+			linlayoutVertical.addView(lilayout);
+		}
 	}
 
 	@Override
@@ -84,8 +66,19 @@ public class ContactListActivity extends Activity {
 			return true;
 		case R.id.act_ContactSearch:
 			startActivity(new Intent(ContactListActivity.this, ContactSearchActivity.class));
+		case R.id.act_AppExit:
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
+	
+	public View.OnClickListener getOnKlickListener(ImageView img){
+	    return new View.OnClickListener() {
+	        public void onClick(View v) {
+	        	Intent in = new Intent(ContactListActivity.this, ChatActivity.class);
+//				in.putExtra("ContactImage", (CharSequence) imgV.getBackground());
+				startActivity(in);
+	        }
+	    };
+}
 }
--- helloProjekt/src/com/Schulprojekt/helloprojekt/GUILogik/ContactListLogik.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/GUILogik/ContactListLogik.java
@@ -2,15 +2,11 @@ package com.Schulprojekt.helloprojekt.GUILogik;
 
 import java.util.ArrayList;
 
-import com.Schulprojekt.helloprojekt.ChatActivity;
-import com.Schulprojekt.helloprojekt.ContactListActivity;
 import com.Schulprojekt.helloprojekt.R;
 
+import android.app.ActionBar.LayoutParams;
 import android.content.Context;
-import android.content.Intent;
-import android.media.Image;
-import android.support.annotation.DrawableRes;
-import android.view.View;
+import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,24 +18,15 @@ public class ContactListLogik {
 	public ArrayList<ContactListEntry> fillList(ArrayList<User> userList, Context con){
 		ArrayList<ContactListEntry> contactList = new ArrayList<ContactListEntry>();
 		for (User user : userList) {
-			
+			i = i+1;
 			LinearLayout linlay = new LinearLayout(con);
 			ImageView img = new ImageView(con);
 			img.setImageResource(R.drawable.dummycontact);
-			//img.setId(user.getAccountName());
 			TextView tv = new TextView(con);
 			tv.setText(user.getAlias());
 			ContactListEntry entry = new ContactListEntry(linlay, img, tv, user.getAccountName());
 			contactList.add(entry);
 		}
 		return contactList;
-	}
-	
-	public View.OnClickListener getOnKlickListener(ImageView img){
-		    return new View.OnClickListener() {
-		        public void onClick(View v) {
-
-		        }
-		    };
 	}
 }
