ece8e51481b2ce631cd3a07ca401526448d628cb
it3-stehch <it3-stehch@R208-22.g18.de>
2015-10-07 10:31:20 +0200

    create MessageServices

---
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/GUILogik/MessageServices.java
@@ -0,0 +1,47 @@
+package com.Schulprojekt.helloprojekt.GUILogik;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+public class MessageServices {
+	
+private static Gson gson = new GsonBuilder().create();
+	
+	/**
+	 * @param args
+	 */
+	public static ArrayList<Message> getMessages(int accountId) {
+		ArrayList<Message> messages = new ArrayList<Message>();
+		String param = "{\"accountId\":\"" + accountId + "\"}";
+
+		HttpResponse response = WebServerUtils.post("/message/GetMessages", param);
+		
+		JsonReader reader;
+		try {
+			reader = new JsonReader(new InputStreamReader(response.getEntity().getContent()));
+			messages = gson.fromJson(reader, new TypeToken<List<Message>>(){}.getType());
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return messages;
+	}
+	
+	public void createMessage(Message message){																			//Deklaration
+		String param = "{\"receiver\":\"" + message.getReceiver() + "\", \"sender\":\"" + message.getSender() + "\", \"messageText\":\"" + message.getMessageText() + "\"}";
+		HttpResponse response = WebServerUtils.post("/user/CreateUser", param);
+	}
+}
