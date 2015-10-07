62d845575647101575ebcdc32209d0e94778b24c
kneska <k.knesel@web.de>
2015-10-07 12:41:17 +0200

    rid

--- helloProjekt/src/com/Schulprojekt/helloprojekt/GUILogik/Relationship.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/GUILogik/Relationship.java
@@ -2,13 +2,23 @@ package com.Schulprojekt.helloprojekt.GUILogik;
 
 
 public class Relationship {																					//hier wird ein Objekt erstellt, dass dem Server übergeben werden soll
+	private int id;
 	private int friendsId;																					//Deklaration
 	private int userId;
 	
 	public Relationship(int friendsId, int userId) {
 		super();
+		this.id = 0;
 		this.friendsId = friendsId;
 		this.userId = userId;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	public int getFriendsId() {
 		return friendsId;
