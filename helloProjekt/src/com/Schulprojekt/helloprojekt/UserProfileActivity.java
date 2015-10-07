f81a02b82e7e81fbd83a9108b729dc4a58c83562
SarahPetzold <xsarahpx@web.de>
2015-10-07 11:25:58 +0200

    UserProfil

--- helloProjekt/src/com/Schulprojekt/helloprojekt/UserProfileActivity.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/UserProfileActivity.java
@@ -25,6 +25,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.Schulprojekt.helloprojekt.GUILogik.Relationship;
+import com.Schulprojekt.helloprojekt.GUILogik.RelationshipServices;
 import com.Schulprojekt.helloprojekt.GUILogik.User;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -71,10 +72,10 @@ public class UserProfileActivity extends Activity {
     	accountname = (TextView) findViewById(R.id.txt_profilAccName);
     	accountname.setText(accountName);
     	
-    	ArrayList<User> friends = new ArrayList<User>();													//Erstellen einer UserArrayList
+    	ArrayList<Relationship> friends = new ArrayList<Relationship>();													//Erstellen einer UserArrayList
 		friends = getFriends();																				//Füllen der ArrayList
-		for (User user : friends) {
-			if(user.getAccountName().equalsIgnoreCase(accountName)){
+		for (Relationship relationship : friends) {
+			if(relationship.getFriendsId() == userId){
 				btn_hinzufuegen.setText("Benutzer löschen");
 			}
 		}
@@ -120,203 +121,26 @@ public class UserProfileActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 	public void fuegeBenutzerHinzu(User loggedUser, User chatPartner){
-		
-		User user = new User();
-		User user2 = new User();
-		
-		DefaultHttpClient httpClient = new DefaultHttpClient();								      			//Client erstellen
-		
-		Gson gson = new Gson();
-		String jsonString = "";
-		jsonString = gson.toJson(loggedUser);
-		StringEntity se;
-		try {
-			se = new StringEntity(jsonString);
 	
-		HttpPost request = new HttpPost(SERVICE_URI+ "/GetUserByAccountName");   							//Auf die Felder AccountName 
-		request.setEntity(se);
-		request.setHeader("Accept", "application/json");
-		request.setHeader("Content-type", "application/json");
-		HttpResponse response;
-		
-			response = httpClient.execute(request);
-			HttpEntity responseEntity = response.getEntity();
-			char[] buffer = new char[(int) responseEntity
-					.getContentLength()]; 																	//Daten im Array speichern
-			InputStream stream = responseEntity.getContent();
-			user = gson.fromJson(stream.toString(), User.class);
-			InputStreamReader reader = new InputStreamReader(stream); 										//Reader deklarieren
-			reader.read(buffer); 																			//Reader liest Buffer
-			stream.close();
-
-			
-		} catch (ClientProtocolException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-
-		Gson gson2 = new Gson();
-		String jsonString2 = "";
-		jsonString2 = gson.toJson(chatPartner);
-		StringEntity se2;
-		try {
-			se2 = new StringEntity(jsonString);
-	
-		HttpPost request2 = new HttpPost(SERVICE_URI+ "/GetUserByAccountName" );  							//Auf die Felder AccountName 
-		request2.setEntity(se2);
-		request2.setHeader("Accept", "application/json");
-		request2.setHeader("Content-type", "application/json");
-		HttpResponse response2;
-			response2 = httpClient.execute(request2);
-			HttpEntity responseEntity = response2.getEntity();
-			char[] buffer = new char[(int) responseEntity
-					.getContentLength()]; 																	//Daten im Array speichern
-			InputStream stream2 = responseEntity.getContent();
-			user2= gson2.fromJson(stream2.toString(), User.class);
-			InputStreamReader reader = new InputStreamReader(stream2); 										//Reader deklarieren
-			reader.read(buffer); 																			//Reader liest Buffer
-			stream2.close();
-
-			
-		} catch (ClientProtocolException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	
-		try {
-		Gson gson3 = new Gson();																			//Erstellen eines GsonObjektes
-		String JsonString3;																					//Erstellen eines JsonStrings
-		Relationship relationship = new Relationship(user2.getAccountID(),user.getAccountID());
-		JsonString3 = gson3.toJson(relationship);
-		DefaultHttpClient httpClient3 = new DefaultHttpClient();
-		HttpPost request3 = new HttpPost(SERVICE_URI + "/CreateRelationship");
-		request3.setHeader("Accept", "application/json");
-        request3.setHeader("Content-type", "application/json");
-        StringEntity se3 = new StringEntity(JsonString3);
-        request3.setEntity(se3);
-        HttpResponse response3 = httpClient3.execute(request3);
-        
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+																			
+		Relationship relationship = new Relationship(userId, userIdLogged);
+		RelationshipServices.createRelationship(relationship);
 
 		}
 	public void loescheBenutzer(User loggedUser, User chatPartner){
 		
-		User user = new User();
-		User user2 = new User();
-		
-		DefaultHttpClient httpClient = new DefaultHttpClient();								      			//Client erstellen
-		
-		Gson gson = new Gson();
-		String jsonString = "";
-		jsonString = gson.toJson(loggedUser);
-		StringEntity se;
-		try {
-			se = new StringEntity(jsonString);
-	
-		HttpPost request = new HttpPost(SERVICE_URI+ "/GetUserByAccountName");   							//Auf die Felder AccountName 
-		request.setEntity(se);
-		request.setHeader("Accept", "application/json");
-		request.setHeader("Content-type", "application/json");
-		HttpResponse response;
-		
-			response = httpClient.execute(request);
-			HttpEntity responseEntity = response.getEntity();
-			char[] buffer = new char[(int) responseEntity
-					.getContentLength()]; 																	//Daten im Array speichern
-			InputStream stream = responseEntity.getContent();
-			user = gson.fromJson(stream.toString(), User.class);
-			InputStreamReader reader = new InputStreamReader(stream); 										//Reader deklarieren
-			reader.read(buffer); 																			//Reader liest Buffer
-			stream.close();
+		Relationship relationship = new Relationship(userId, userIdLogged);
+		RelationshipServices.deleteRelationship(relationship);
 
-			
-		} catch (ClientProtocolException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-
-		Gson gson2 = new Gson();
-		String jsonString2 = "";
-		jsonString2 = gson.toJson(chatPartner);
-		StringEntity se2;
-		try {
-			se2 = new StringEntity(jsonString);
-	
-		HttpPost request2 = new HttpPost(SERVICE_URI+ "/GetUserByAccountName" );  							//Auf die Felder AccountName 
-		request2.setEntity(se2);
-		request2.setHeader("Accept", "application/json");
-		request2.setHeader("Content-type", "application/json");
-		HttpResponse response2;
-			response2 = httpClient.execute(request2);
-			HttpEntity responseEntity = response2.getEntity();
-			char[] buffer = new char[(int) responseEntity
-					.getContentLength()]; 																	//Daten im Array speichern
-			InputStream stream2 = responseEntity.getContent();
-			user2= gson2.fromJson(stream2.toString(), User.class);
-			InputStreamReader reader = new InputStreamReader(stream2); 										//Reader deklarieren
-			reader.read(buffer); 																			//Reader liest Buffer
-			stream2.close();
-
-			
-		} catch (ClientProtocolException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		
 		}
 	
-		try {
-		Gson gson3 = new Gson();																			//Erstellen eines GsonObjektes
-		String JsonString3;																					//Erstellen eines JsonStrings
-		Relationship relationship = new Relationship(user2.getAccountID(),user.getAccountID());
-		JsonString3 = gson3.toJson(relationship);
-		DefaultHttpClient httpClient3 = new DefaultHttpClient();
-		HttpPost request3 = new HttpPost(SERVICE_URI + "/DeleteRelationship");
-		request3.setHeader("Accept", "application/json");
-        request3.setHeader("Content-type", "application/json");
-        StringEntity se3 = new StringEntity(JsonString3);
-        request3.setEntity(se3);
-        HttpResponse response3 = httpClient3.execute(request3);
-        
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		}
-	 public ArrayList<User> getFriends(){																		//Erstellen der Methode getFriends
-	    	ArrayList<User> friends = new ArrayList<User>();													//Erstellen einer ArrayList
-	    	Gson gs = new Gson();																				//Erstellen eines GsonObjektes
-	    	User user = new User();
-	    	String jsonString = "";																				//Erstellen eines JsonStrings
-				jsonString = gs.toJson(user.getAccountID());													//Füllen des JsonStrings
-				StringEntity se;																				//Erstellen eines StringEntity
-				try {
-					se = new StringEntity(jsonString);															//Füllen des StringEntitys
-				DefaultHttpClient httpClient = new DefaultHttpClient();											//Erstellen des HttpClients
-				HttpPost request = new HttpPost(SERVICE_URI+ "/GetRelationship");    							//Aufrufen der Methode GetRelationship
-				request.setEntity(se);
-				request.setHeader("Accept", "application/json");
-				request.setHeader("Content-type", "application/json");
-				HttpResponse response = null;
-				response = httpClient.execute(request);
-				HttpEntity responseEntity = response.getEntity();
-				InputStream stream = null;
-				stream = responseEntity.getContent();
-				friends = gs.fromJson(stream.toString(), new TypeToken<List<User>>(){}.getType());				//Füllen der ArrayList friends
-				} catch (IllegalStateException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				return friends;
+	public ArrayList<Relationship> getFriends(){																		//Erstellen der Methode getFriends
+	    	ArrayList<Relationship> friends = new ArrayList<Relationship>();													//Erstellen einer ArrayList
+	    	
+	    	friends = RelationshipServices.getRelationship(userIdLogged);
+	    	
+			return friends;
 	    }
 }
 
