2d6fd458813e3ebdd2c6875c003900d5126b678f
SarahPetzold <xsarahpx@web.de>
2015-10-08 10:27:41 +0200

    Registration

--- helloProjekt/src/com/Schulprojekt/helloprojekt/RegistrationActivity.java
+++ helloProjekt/src/com/Schulprojekt/helloprojekt/RegistrationActivity.java
@@ -1,28 +1,9 @@
 package com.Schulprojekt.helloprojekt;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,13 +15,10 @@ import android.widget.Toast;
 import com.Schulprojekt.helloprojekt.GUILogik.User;
 import com.Schulprojekt.helloprojekt.GUILogik.UserServices;
 import com.Schulprojekt.helloprojekt.GUILogik.md5Generator;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
 
 
 public class RegistrationActivity extends Activity {
-	public Button bregistrieren;		//Deklaration
+	public Button bregistrieren;																				//Deklaration
 	public EditText tvname;
 	public EditText tvalias;
 	public EditText tvpasswort;
@@ -54,48 +32,37 @@ public class RegistrationActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         
-        bregistrieren = (Button) findViewById(R.id.registrieren);												//auf Button register zurgreifen
-        bregistrieren.setOnClickListener(new OnClickListener() {												//auf den Button register einen OnClickListener setzen
+        bregistrieren = (Button) findViewById(R.id.registrieren);												//Auf Button register zugreifen
+        bregistrieren.setOnClickListener(new OnClickListener() {												//Auf den Button register einen OnClickListener setzen
         	public void onClick(View view) {
         		if(view == bregistrieren) {
-        			tvname = (EditText) findViewById(R.id.username);											//auf Textfeld username zugreifen
-        			tvalias = (EditText) findViewById(R.id.username);											//auf Textfeld username zugreifen - bei der Registrierung entspricht der Alias dem Username
-        			tvpasswort = (EditText) findViewById(R.id.passwort);										//auf Textfeld passwort zugreifen
-        			tvpasswortwh = (EditText) findViewById(R.id.passwortwh);									//auf Textfeld passwortwh zugreifen
+        			tvname = (EditText) findViewById(R.id.username);											//Auf Textfeld username zugreifen
+        			tvalias = (EditText) findViewById(R.id.username);											//Auf Textfeld username zugreifen - bei der Registrierung entspricht der Alias dem Username
+        			tvpasswort = (EditText) findViewById(R.id.passwort);										//Auf Textfeld passwort zugreifen
+        			tvpasswortwh = (EditText) findViewById(R.id.passwortwh);									//Auf Textfeld passwortwh zugreifen
         			String name = tvname.getText().toString();													//tvname in einen String umwandeln und in dem String name speichern
         			String alias = tvalias.getText().toString();												//tvalias in einen String umwandeln und in dem String alias speichern
         			String passwort = tvpasswort.getText().toString();											//tvpasswort in einen String umwandeln und in dem String passwort speichern
         			String passwortwh = tvpasswortwh.getText().toString();										//tvpasswortwh in einen String umwandeln und in dem String passwortwh speichern
         			
-        			
-        			// TODO alias muss beim create noch auf client oder serverseite gleich dem accountName gesetzt werden!
-        			
-        			
         			if(name.equals("") || alias.equals(null) 
-        					|| passwort.equals("") || passwort.equals(null)){									//prüfen ob name oder passwort leer sind
+        					|| passwort.equals("") || passwort.equals(null)){									//Prüfen ob name oder passwort leer sind
         				Toast.makeText(RegistrationActivity.this, "Es werden alle Eingaben Benötigt", 
         						Toast.LENGTH_LONG).show();
         			}else{
-        				if(passwortwh.equals(passwort)){														//prüfen ob passwortwh passwort entspricht
+        				if(passwortwh.equals(passwort)){														//Prüfen ob passwortwh passwort entspricht
     						try{
     							user = UserServices.getUserByAccountName(name);
-    							if (user.getAccountName() == null) { 											// ist loginUsername null, kommt die Fehlermeldung
+    							if (user.getAccountName() == null) { 											//Wenn loginUsername null ist, kommt die Fehlermeldung
     								md5Pass = md5Generator.getMd5(passwort);
     								user = UserServices.createUser(name, md5Pass);
     								
-    								Intent i = new Intent(RegistrationActivity.this, 						// sonst wird die nächste Activity ContactListActivity gestartet
+    								Intent i = new Intent(RegistrationActivity.this, 							//Sonst wird die nächste Activity "ContactListActivity" gestartet
     										ContactListActivity.class);
     								Bundle b = new Bundle();
     								b.putInt("userId", user.getAccountID());
     								b.putString("aliasName", user.getAlias());
     								b.putString("accountName", user.getAccountName());
-//    								
-//    								Bitmap bmp = ((BitmapDrawable)d).getBitmap();
-//    								ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//    								bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
-//    								byte[] byteArray = baos.toByteArray();
-//    								
-//    								b.putByteArray("picture", byteArray);
     								b.putString("password", user.getPassword());
     								i.putExtras(b);
     								startActivity(i);
@@ -115,7 +82,6 @@ public class RegistrationActivity extends Activity {
         					Toast.makeText(RegistrationActivity.this, "Passwörter stimmen nicht überein", 		
         							Toast.LENGTH_LONG).show();
         				}
-
         			} 
         			}       					
         	}
@@ -124,20 +90,15 @@ public class RegistrationActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {																//Menü wird aufgebaut
-        //getMenuInflater().inflate(R.menu.registration, menu);
-        return true;																							//wenn Menü aufgebaut ist, gibt die Methode true zurück
+        return true;																							//Wenn Menü aufgebaut ist, gibt die Methode true zurück
     }
     
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {														//wird ein Item im Menüe ausgewählt, gibt die Methode true zurück
+    public boolean onOptionsItemSelected(MenuItem item) {														//Wird ein Item im Menüe ausgewählt, gibt die Methode true zurück
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    
-
-    
-
 }
